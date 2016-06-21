9bb8f6a434921f41de657e2fb97f1ad199ec2b1b
ML <moti.l@mediapplications.com>
2016-06-21 13:00:12 +0300

    MarqueeText

--- boilerplate/vanity-website/src/components/HideousHomePage.js
+++ boilerplate/vanity-website/src/components/HideousHomePage.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import * as actions from '../store/actions'
-import {store} from '../store'
 export class HideousHomePage extends React.Component {
 
-  /*
-  TODO - increment the visitor count by dispatching a redux action
-  You'll need: import * as actions from '../store/actions';
-  */
   constructor(props) {
     super(props);
     this.state = {
       marqueeText: 'Chimpanzees with their tongues out are cool. I wonder if they are equally enamored by our antics.'
-    }
-
-    store.dispatch(actions.incrementVisitorCount());
+    };
   }
 
+  componentDidMount() {
+    this.props.dispatch(actions.incrementVisitorCount());
+  }
 
   render() {
     return (
@@ -28,7 +24,9 @@ export class HideousHomePage extends React.Component {
           <a href="#/monkeys">Check out some cool pictures!!</a>
         </div>
         <div>
-          <input placeholder="Update marquee text..." /> {/*onChange: update marqueeText to be event.target.value*/}
+          <input placeholder="Update marquee text..." onChange={(event) => {
+            this.setState({marqueeText: event.target.value});
+          }}/>
         </div>
         <marquee>{this.state.marqueeText}</marquee>
       </div>
--- boilerplate/vanity-website/src/components/MonkeyList.js
+++ boilerplate/vanity-website/src/components/MonkeyList.js
@@ -5,6 +5,7 @@ import React from 'react';
 export const MonkeyList = () =>
   <div className="monkey-list">
     <a href="#/">Back to Home Page</a>
+    
     /* render a list of images with width 120 each
     * clicking on the image should navigate to '#/monkeys/:index'
     * Create a dedicated component called Image to render each image in a new line
