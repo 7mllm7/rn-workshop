1b2436f5c97721bbea58b2156bf467770804f267
ML <moti.l@mediapplications.com>
2016-06-21 21:43:57 +0300

    1

--- solution/VanityApp/src/components/MonkeyList.js
+++ solution/VanityApp/src/components/MonkeyList.js
@@ -1,51 +1,75 @@
-import React, {Component} from 'react';
-import {View, StyleSheet, TouchableOpacity, Image, ListView} from 'react-native';
-import BackButton from './BackButton';
+/**
+ * Created by MLmediapps on 21/06/2016.
+ */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    ListView,
+    TouchableHighlight
+} from 'react-native';
 
-const datasource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-const ds = datasource.cloneWithRows([
-  require('../../images/monkey1.jpeg'),
-  require('../../images/monkey2.jpeg'),
-  require('../../images/monkey3.jpeg'),
-  require('../../images/monkey4.jpeg'),
-  require('../../images/monkey1.jpeg'),
-  require('../../images/monkey6.jpeg'),
-  require('../../images/monkey7.jpeg'),
-  require('../../images/monkey8.jpeg'),
-  require('../../images/monkey9.jpeg'),
+const images = [
+    require('../../images/monkey1.jpeg'),
+    require('../../images/monkey2.jpeg'),
+    require('../../images/monkey3.jpeg'),
+    require('../../images/monkey4.jpeg'),
+    require('../../images/monkey5.jpeg'),
+    require('../../images/monkey6.jpeg'),
+    require('../../images/monkey7.jpeg'),
+    require('../../images/monkey8.jpeg'),
+    require('../../images/monkey9.jpeg')
+];
 
-]);
-
-const renderRow = (image, navigateToMonkey) =>
-  <TouchableOpacity
-    style={styles.row}
-    onPress={() => navigateToMonkey(image)}>
-    <Image source={image} style={styles.image}/>
-  </TouchableOpacity>;
-
-const MonkeyList = ({back, navigateToMonkey}) =>
-  <View style={styles.container}>
-    <BackButton back={back}/>
-    <ListView dataSource={ds} renderRow={image => renderRow(image, navigateToMonkey)} />
-  </View>
-;
-
-export default MonkeyList;
+export class MonkeyList extends Component {
+    constructor(props) {
+        super(props);
+        var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            dataSource: ds.cloneWithRows([
+                '1',
+                '2',
+                '3',
+                '4',
+                '5',
+                '6',
+                '7',
+                '8',
+                '9'
+            ]),
+        };
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <ListView
+                    style={{flex:1}}
+                    dataSource={this.state.dataSource}
+                    renderRow={(rowData) => <TouchableHighlight onPress={()=>{this.props.onMonkeyPressed(images[rowData])}} style={styles.container}><Image source={images[rowData]} /></TouchableHighlight>}
+                />
+                <TouchableHighlight onPress={this.props.onBack}>
+                    <Text>Back!</Text>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: 20,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(123, 101, 200, .7)'
-  },
-  row: {
-    margin: 5
-  },
-  image: {
-    height: 200,
-    width: 200
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+
 });
+
--- solution/vanity-website/src/index.js
+++ solution/vanity-website/src/index.js
@@ -4,17 +4,16 @@ import ReactDOM from 'react-dom';
 import {connect} from 'react-redux';
 import {Provider} from 'react-redux';
 import {store, mapStateToProps} from './store';
-import { Router, Route, hashHistory } from 'react-router'
+import {Router, Route, hashHistory} from 'react-router'
 import {HideousHomePage} from './components/HideousHomePage';
 import {MonkeyList} from './components/MonkeyList';
 import {Monkey} from './components/Monkey';
 
+require('normalize.css/normalize.css');
+require('styles/App.scss');
 
 const ConnectedHideousHomePage = connect(mapStateToProps)(HideousHomePage);
 const ConnectedMonkeyList = connect(mapStateToProps)(MonkeyList);
-
-require('normalize.css/normalize.css');
-require('styles/App.scss');
 
 ReactDOM.render(
   <Provider store={store}>
