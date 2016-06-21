5412a9d7c54965f054f63941a6c948b14bb02180
ML <moti.l@mediapplications.com>
2016-06-21 13:35:21 +0300

    moneky list + link

--- boilerplate/vanity-website/src/components/MonkeyList.js
+++ boilerplate/vanity-website/src/components/MonkeyList.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Image from './Image'
 
-//const images = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const images = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export const MonkeyList = () =>
   <div className="monkey-list">
     <a href="#/">Back to Home Page</a>
-    
+
+    {
+      images.map((index) =>
+        <div key={index}>
+          <a href={`#/monkeys/${index}`}><Image image={`../images/monkey${index}.jpeg`}/> </a>
+        </div>
+      )
+    }
     /* render a list of images with width 120 each
     * clicking on the image should navigate to '#/monkeys/:index'
     * Create a dedicated component called Image to render each image in a new line
